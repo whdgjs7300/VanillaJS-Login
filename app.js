2608d9851225a1102597b0d0df3ce7d08807e92e
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-02-03 15:28:19 +0900

    2/3 event2

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ const loginInput = loginForm.querySelector("#login-form input");
 const loginButton = loginForm.querySelector("#login-form button");
 
 function onLoginBtnClick() {
+    const username = loginInput.value;
+    if (username === "" ) {
+        alert("이름을 입력해주세요");
+    } else if(username.length > 15) {
+        alert("이름이 너무 길어요");
+    }
     
-    console.log("클릭");
 }
 
 

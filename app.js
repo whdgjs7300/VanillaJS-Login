074f8a35f8ab831a8c9870f43c4234c17b8a2d33
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-02-06 18:34:29 +0900

    2/6 Event2

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("#login-form input");
 
+const link = document.querySelector("a");
+
+
 // form submit은 새로고침이 일어나는 기본동작
 
 function onLoginSubmit(e) {
@@ -9,5 +12,10 @@ function onLoginSubmit(e) {
     e.preventDefault();
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function handleclick(e) {
+    // 브라우저 기본동작을 막음
+    e.preventDefault();
+}
 
+loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click",handleclick);

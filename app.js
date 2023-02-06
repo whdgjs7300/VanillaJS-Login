4943b32d36e0faf2e37802a4353b10809f4af4af
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-02-06 19:11:07 +0900

    2/6 localstorage

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ function onLoginSubmit(e) {
     // 새로고침을 막아 데이터가 유지되도록 함
     e.preventDefault();
     const username = loginInput.value;
+    // 로컬스토리지에 유저네임 저장
+    localStorage.setItem("username", username);
     loginForm.classList.add(HIDDEN_CLASSNAME);
     greeting.innerHTML = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);

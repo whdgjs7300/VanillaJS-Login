e974bd61ac8090998aa8b404a4091b7ce6ba5e16
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-02-08 20:08:02 +0900

    2/8 clock

--- js/clock.js
+++ js/clock.js
@@ -4,7 +4,14 @@ clock.innerHTML = "kakaka";
 
 // interval 매번 일어나야하는 일 ex) 2초마다 실행하고 싶은 함수
 
-function sayHello() {
-    
+function getClock() {
+    const date = new Date();
+    clock.innerHTML = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} `
 }
-setInterval(sayHello, 5000);
+// 먼저 함수를 즉시 호출한 후에 셋인터벌 실행
+// 1초전에 기존의 HTML 텍스트 형식 보여주기 방지
+getClock()
+setInterval(getClock, 1000);
+
+// setTimeout = 실행 후 동작 종료
+

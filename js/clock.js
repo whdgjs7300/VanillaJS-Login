8028a7b9e7b84d58f53e51b0626db6df259967c5
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-02-08 19:56:10 +0900

    2/8 setInterval

---
+++ js/clock.js
@@ -0,0 +1,10 @@
+const clock = document.querySelector("#clock");
+
+clock.innerHTML = "kakaka";
+
+// interval 매번 일어나야하는 일 ex) 2초마다 실행하고 싶은 함수
+
+function sayHello() {
+    
+}
+setInterval(sayHello, 5000);

6d0e9e01847d0737c95b84e42a1ba83338ab8646
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-02-11 20:15:10 +0900

    2/11 addTodo

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,20 @@ const todoForm = document.querySelector("#todo-form");
 const todoInput = todoForm.querySelector("#todo-form input");
 const todoList = document.querySelector("#todo-list");
 
+function paintTodo(newTodo) {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    li.appendChild(span);
+    span.innerHTML = newTodo;
+    todoList.appendChild(li);
+}
+
+
 function handleTodoSubmit(e) {
     e.preventDefault();
     const newTodo = todoInput.value;
     todoInput.value = "";
+    paintTodo(newTodo);
 }
 
 todoForm.addEventListener("submit", handleTodoSubmit);

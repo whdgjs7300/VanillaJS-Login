df1d97393d51890d0131bcc0d7665ff8ed6cf0f9
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-02-11 20:28:37 +0900

    2/11 removeTodo

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,18 @@ const todoForm = document.querySelector("#todo-form");
 const todoInput = todoForm.querySelector("#todo-form input");
 const todoList = document.querySelector("#todo-list");
 
+function deleteTodo(e) {
+    e.target.parentElement.remove();
+}
+
 function paintTodo(newTodo) {
     const li = document.createElement("li");
     const span = document.createElement("span");
+    const button = document.createElement("button");
+    button.innerHTML = "X"
+    button.addEventListener("click", deleteTodo);
     li.appendChild(span);
+    li.appendChild(button);
     span.innerHTML = newTodo;
     todoList.appendChild(li);
 }

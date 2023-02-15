b5514ffc5adcacc055677fe5dc923973a277c783
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-02-15 17:26:29 +0900

    2/15 MiniProject

--- js/background.js
+++ js/background.js
@@ -4,6 +4,7 @@ const chosenImage =  images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
 bgImage.src = `img/${chosenImage}`;
+bgImage.classList.add("bgImg");
 
 console.log(bgImage);
 
